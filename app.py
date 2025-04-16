12215e1425bf558c10e8a1fde7f353c639a4d9ec
marzgod <starofmaria@gmail.com>
2025-04-15 21:01:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ client = tweepy.Client(
     consumer_key=os.getenv("API_KEY"),
     consumer_secret=os.getenv("API_SECRET"),
     access_token=os.getenv("ACCESS_TOKEN"),
-    access_token_secret=os.getenv("ACCESS_TOKEN_SECRET")
+    access_token_secret=os.getenv("ACCESS_SECRET")
 )
 
 @app.route('/')
